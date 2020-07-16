c1dfafb4e5a829a6a421e8f07aff6fab56e00c47
Vitaliy <michael8913@gmail.com>
2020-07-16 09:42:58 +0300

    log out

--- client/src/Components/Router.jsx
+++ client/src/Components/Router.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import PropTypes from "prop-types";
 import Auth from "../Routes/Auth";
 import Feed from "../Routes/Feed";
@@ -13,9 +13,15 @@ const LoggedInRoutes = () => (
         <Route path="/explore" component={Explore}/>
         <Route path="/search" component={Search}/>
         <Route path="/:username" component={Profile}/>
+        <Redirect from="*" to="/" />
     </Switch>
 );
-const LoggedOutRoutes = () => <Switch><Route exact path="/" component={Auth}/></Switch>;
+const LoggedOutRoutes = () => (
+    <Switch>
+        <Route exact path="/" component={Auth} />
+        <Redirect from="*" to="/" />
+    </Switch>
+);
 
 const AppRouter = ({ isLoggedIn }) => isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />;
 
